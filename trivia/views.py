09ff7e8bb860f2b36495b634f98512066770f667
Joaquin de Castro <63779121+JoaquindeCastro@users.noreply.github.com>
2021-03-31 21:14:36 +0800

    Update views.py

--- trivia/views.py
+++ trivia/views.py
@@ -58,7 +58,7 @@ class PostFactView(APIView):
 class TriviaEmailView(APIView):
 
 	def post(self,request,format=None):
-		pwd = request.query_params.post('pwd', None)
+		pwd = request.POST.get('pwd', None)
 		if pwd == 'hjdgjwerifj49':
 			#category = request.query_params.post('cartegory', None)
 			trivia = random.choice(Fact.objects.all())
