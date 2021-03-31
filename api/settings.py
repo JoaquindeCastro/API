f3fcf733469c884e3c4df16ba69febb6833295c6
Joaquin de Castro <63779121+JoaquindeCastro@users.noreply.github.com>
2021-03-31 21:36:51 +0800

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -136,9 +136,9 @@ MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST
