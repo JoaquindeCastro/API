f1670661b892a34bea267005e35ad3b50008230a
Joaquin de Castro <63779121+JoaquindeCastro@users.noreply.github.com>
2021-03-31 15:02:35 +0800

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',include('main.urls')),
     path('users/', include('users.urls')),
     path('quotes/',include('quotes.urls')),
+    path('trivia/',include('trivia.urls')),
     path('issuetracker/',include('issuetracker.urls')),
 ]
