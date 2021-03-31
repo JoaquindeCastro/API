f3f7f1f65345ab6c15a7f07de9703a5dd146b2c7
Joaquin de Castro <63779121+JoaquindeCastro@users.noreply.github.com>
2021-03-31 15:10:06 +0800

    Update urls.py

--- trivia/urls.py
+++ trivia/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('fact', views.RandomFactView.as_view(), name='random_fact'),
+	path('fact/post',views.PostFactView.as_view(),name='post_fact'),
 	path('question', views.RandomQuestionView.as_view(), name='random_question')
 ]
